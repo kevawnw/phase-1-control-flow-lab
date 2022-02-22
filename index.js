c97c8b9c55b81c661ccd33554fc3cfb71b701087
Kevawn Warren <kevawnw@gmail.com>
2022-02-22 17:50:10 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(aNumber){
+  if (aNumber <= 400) {
+    return "This one is on me!"
+  } else if (aNumber > 2000 && aNumber <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (aNumber > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  if (someCity === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  if (tips === "generous") {
+    return "Thank you so much."
+  } else if (tips === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
